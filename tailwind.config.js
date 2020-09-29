89d4beba89a36c1d05d405f4240132dc650b44e6
Lucas <lucas.bailly02@edu.univ-fcomte.fr>
2020-09-29 15:21:42 +0200

    media query tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,15 @@ module.exports = {
     extend: {
       colors: {
         'gold': '#FFD700',
+        'red' : '#BF0A30',
+      },
+      screens: {
+        'portrait': {'raw': '(orientation: portrait)'},
+        'dark': {'raw': '(prefers-color-scheme: dark)'},
+        // => @media (orientation: portrait) { ... }
       }
     },
   },
   variants: {},
   plugins: [],
-}
+};
